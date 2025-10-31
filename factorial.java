18fbe40ac50f0c53b4c6cbe4400da1944179090c
Abhishek Bhat <bhatabhishek409@gmail.com>
2025-10-31 17:22:05 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class factorial {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number to find the factorial");
+        int n=sc.nextInt();
+        int factorial=1;
+        for(int i=1; i<=n; i++){
+            factorial=factorial*i;
+        }
+        System.out.println("Factorial of a number is " +factorial);
+        sc.close();
+    }
+}
